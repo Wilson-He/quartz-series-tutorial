d87d18e0e1e9b1cdb3183e6fdc6ea3092fca1f54
Wilson-He <845023508@qq.com>
2019-04-14 23:34:50 +0800

    init

---
+++ class-a/src/main/java/io/github/quartz/HelloJob.java
@@ -0,0 +1,19 @@
+package io.github.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * HelloJob
+ *
+ * @author Wilson
+ * @date 2019/4/14
+ */
+public class HelloJob implements Job {
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        System.out.println(context.getTrigger().getJobKey());
+        System.out.println("Hello Job");
+    }
+}
